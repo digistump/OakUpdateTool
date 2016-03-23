5a77443dee2b6236feba0a098290f82241e8152b
Erik Tylek Kettenburg <digistump@gmail.com>
2016-03-23 14:54:41 -0700

    connect to digistump for fallback ip check since it has to be up anyway

--- oakupsrv.py
+++ oakupsrv.py
@@ -28,7 +28,7 @@ def getHost():
 		return ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 	except:
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		s.connect(("gmail.com",80))
+		s.connect(("digistump.com",80))
 		ip = s.getsockname()[0]
 		s.close()
 		return ip
